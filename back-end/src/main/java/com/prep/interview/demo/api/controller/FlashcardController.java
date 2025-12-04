6fee1cc6f3e94a98c756a7ee36e04289c37641af
Max <wiegantmax@gmail.com>
2025-12-04 09:41:13 -0700

    added an endpoint to get specific flashcard

--- back-end/src/main/java/com/prep/interview/demo/api/controller/FlashcardController.java
+++ back-end/src/main/java/com/prep/interview/demo/api/controller/FlashcardController.java
@@ -1,6 +1,11 @@
 package com.prep.interview.demo.api.controller;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prep.interview.demo.persistence.entity.Flashcard;
@@ -15,9 +20,24 @@ public class FlashcardController {
 		this.flashcardRepository = flashcardRepository;
 	}
 	
-	
 	@GetMapping("/flashcards")
 	public Iterable<Flashcard> findAllFlashcards() {
 		return this.flashcardRepository.findAll();
+	}
+	
+	@GetMapping("/questions")
+	public Flashcard findQuestion(@RequestParam String id) {
+		Flashcard question = null;
+		
+		try {
+			Integer intId = Integer.parseInt(id);
+			Optional<Flashcard> flashcard = this.flashcardRepository.findById(intId);
+			
+			if (flashcard.isPresent())
+				question = flashcard.get();
+			
+		} catch (NumberFormatException ex) {}
+		
+		return question;
 	}
 }
--- back-end/src/main/java/com/prep/interview/demo/persistence/repository/FlashcardRepository.java
+++ back-end/src/main/java/com/prep/interview/demo/persistence/repository/FlashcardRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.prep.interview.demo.persistence.entity.Flashcard;
 
-public interface FlashcardRepository extends CrudRepository<Flashcard, Integer>{}
+public interface FlashcardRepository extends CrudRepository<Flashcard, Integer>{
+
+}
